f7de9c6fdf6b40078eb664931ef121e20b7a9432
AlexRudnyk <alexander.rudnyk@ukr.net>
2022-09-01 20:15:21 +0300

    Task color-switcher done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const PROMPT_DELAY = 1000;
 
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+startBtn.addEventListener('click', colorize);
+stopBtn.addEventListener('click', stopColorize);
+
+let intervalId = null;
+
+function colorize() {
+  startBtn.setAttribute('disabled', 'disabled');
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, PROMPT_DELAY);
+}
+
+function stopColorize() {
+  clearInterval(intervalId);
+  startBtn.removeAttribute('disabled', 'disabled');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
